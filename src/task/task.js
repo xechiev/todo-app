0a6d1804dc67ff0ec14ae795869d40c56e9f1c9c
Mingiian Khechiev <58348380+xechiev@users.noreply.github.com>
2021-06-20 14:50:16 +0300

    Исправил классовый компонент на функциональный

--- src/task/task.js
+++ src/task/task.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import PropTypes from 'prop-types';
 
@@ -6,37 +6,37 @@ import './task.css';
 
 
 
-export default class Task extends Component {
+const Task = ({ label, done, timeCreate, onDeleted, onToggleDone }) => {
 
-   static propTypes = {
-      label: PropTypes.string.isRequired,
-      done: PropTypes.bool.isRequired,
-      timeCreate: PropTypes.number.isRequired,
-      onDeleted: PropTypes.func.isRequired,
-      onToggleDone: PropTypes.func.isRequired
-   }
+   const whenCreated = formatDistanceToNow(timeCreate, { includeSeconds: true });
+   const checkItem = done ? 'checked' : '';
+   let doneItem = ['description'];
+   if(done) doneItem.push('done')
 
-   render() {
-      const { label, done, timeCreate, onDeleted, onToggleDone} = this.props;
-      const whenCreated = formatDistanceToNow(timeCreate, { includeSeconds: true });
-      const checkItem = done ? 'checked' : '';
-      let doneItem = ['description'];
-      if(done) doneItem.push('done')
-
-      return (
-         <div >
-            <input className="toggle" 
-                   type="checkbox" 
-                   onClick={onToggleDone}
-                   defaultChecked={checkItem}
-            />
-            <label>
-               <span className={doneItem.join(' ')}> {label} </span>   
-               <span className="created">created {whenCreated} ago</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy" onClick={onDeleted}></button>
-         </div>
-      );
-   }
+   return (
+      <div >
+         <input className="toggle" 
+               type="checkbox" 
+               onClick={onToggleDone}
+               defaultChecked={checkItem}
+         />
+         <label>
+            <span className={doneItem.join(' ')}> {label} </span>   
+            <span className="created">created {whenCreated} ago</span>
+         </label>
+         <button className="icon icon-edit"></button>
+         <button className="icon icon-destroy" onClick={onDeleted}></button>
+      </div>
+   );
 }
+
+Task.propTypes = {
+   label: PropTypes.string.isRequired,
+   done: PropTypes.bool.isRequired,
+   timeCreate: PropTypes.number.isRequired,
+   onDeleted: PropTypes.func.isRequired,
+   onToggleDone: PropTypes.func.isRequired
+}
+
+
+export default Task;
