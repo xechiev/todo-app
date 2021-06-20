be6067787294ae5f97c892713a18b6d2d7ba2900
Mingiian Khechiev <58348380+xechiev@users.noreply.github.com>
2021-06-20 14:49:43 +0300

    Исправил классовый компонент на функциональный

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TaskFilter from '../task-filter';
 import PropTypes from 'prop-types';
 
 import './footer.css';
 
-export default class Footer extends Component {
+const Footer = ({ todos, itemFilter, onClearCompleted, onfilterTodos}) => {
 
-   static propTypes = {
-      todos: PropTypes.arrayOf(PropTypes.object).isRequired,
-      itemFilter: PropTypes.arrayOf(PropTypes.string),
-      onClearCompleted: PropTypes.func.isRequired,
-      onfilterTodos: PropTypes.func.isRequired,
-   }
+   const doneCount = todos.filter((el) => !el.done).length;
 
-   render() {
-      const { todos, itemFilter, onClearCompleted, onfilterTodos} = this.props
-
-      const doneCount = todos.filter((el) => !el.done).length;
-
-      return (
-         <footer className="footer">
-            <span className="todo-count">{doneCount} items left</span>
-            <TaskFilter 
+   return (
+      <footer className="footer">
+         <span className="todo-count">{doneCount} items left</span>
+         <TaskFilter 
                onfilterTodos={onfilterTodos}
                itemFilter={itemFilter}
-            />
-            <button 
-               className="clear-completed"
-               onClick={onClearCompleted}>
-               Clear completed
-            </button>
-         </footer>
-      );
-   }
+         />
+         <button 
+            className="clear-completed"
+            onClick={onClearCompleted}>
+            Clear completed
+         </button>
+      </footer>
+   );
 }
 
+Footer.propTypes = {
+   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+   itemFilter: PropTypes.string,
+   onClearCompleted: PropTypes.func.isRequired,
+   onfilterTodos: PropTypes.func.isRequired,
+}
+
+export default Footer;
